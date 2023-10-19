2586ef890fd24e9cd3edca273c3b7e5695d5cec0
Jefferson2023-git <jeffernaviiab@outlook.com>
2023-10-19 07:16:04 -0500

    Perro

---
+++ app/src/main/java/com/example/animation2/Perro.java
@@ -0,0 +1,28 @@
+package com.example.animation2;
+
+public class Perro {
+
+    String nombre;
+    double altura;
+    double peso;
+    String colorPelaje;
+    String genero;
+    String raza;
+
+
+    public Perro(String nombre, double altura, double peso, String colorPelaje, String genero, String raza) {
+        this.nombre = nombre;
+        this.altura = altura;
+        this.peso = peso;
+        this.colorPelaje = colorPelaje;
+        this.genero = genero;
+        this.raza = raza;
+    }
+
+
+    }
+
+
+
+
+
